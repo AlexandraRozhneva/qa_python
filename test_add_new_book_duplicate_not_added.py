8b07581d2544af968b03d3f076bc7c8894c4340f
AlexandraRozhneva <wolchica27@yandex.ru>
2026-05-24 15:56:40 +0300

    Delete test_add_new_book_duplicate_not_added.py

--- test_add_new_book_duplicate_not_added.py
+++
@@ -1,9 +0,0 @@
-from main import BooksCollector
-
-class TestBooksCollector:
-
-    def test_add_new_book_duplicate_not_added(self):
-        collector = BooksCollector()
-        collector.add_new_book("Война и мир")
-        collector.add_new_book("Война и мир")
-        assert len(collector.books_genre) == 1
