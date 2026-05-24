fa6eaf73202c1799916cb4840eb3f10167f4dd12
AlexandraRozhneva <wolchica27@yandex.ru>
2026-05-24 15:59:11 +0300

    Delete test_get_list_of_favorites_books_empty.py

--- test_get_list_of_favorites_books_empty.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from main import BooksCollector
-
-class TestBooksCollector:
-
-    def test_get_list_of_favorites_books_empty(self):
-        collector = BooksCollector()
-        assert collector.get_list_of_favorites_books() == []
