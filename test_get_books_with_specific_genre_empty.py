3e1a01436a900c68d6b19cf80292327005c444ef
AlexandraRozhneva <wolchica27@yandex.ru>
2026-05-24 15:58:41 +0300

    Delete test_get_books_with_specific_genre_empty.py

--- test_get_books_with_specific_genre_empty.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from main import BooksCollector
-
-class TestBooksCollector:
-
-    def test_get_books_with_specific_genre_empty(self):
-        collector = BooksCollector()
-        result = collector.get_books_with_specific_genre("Фантастика")
-        assert result == []
