0a4fc42ab34ef8a28a443c3c8ddf0eda48100e3b
AlexandraRozhneva <wolchica27@yandex.ru>
2026-05-24 15:57:52 +0300

    Delete test_get_book_genre_not_exists.py

--- test_get_book_genre_not_exists.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from main import BooksCollector
-
-class TestBooksCollector:
-
-    def test_get_book_genre_not_exists(self):
-        collector = BooksCollector()
-        assert collector.get_book_genre("Несуществующая книга") is None
